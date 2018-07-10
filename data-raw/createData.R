6e212d9c84747e62655d50992405f8b9fdcd56da
David Jimenez-Morales <biodavidjm@gmail.com>
2018-07-09 20:59:42 -0700

    updates in data
    #1

--- data-raw/createData.R
+++ data-raw/createData.R
@@ -4,13 +4,13 @@ setwd("~/github/biodavidjm/artMS/")
 
 # GENERATE RANDOM FILE
 randomDF <- data.frame(replicate(10,sample(0:1,100,rep=TRUE)))
-save(randomDF, file = 'data/randomDF.Rdata')
+save(randomDF, file = 'data/randomDF.rdata')
 
 # PH FILES
 # MaxQuant Evidence file
 ph_evidence <- read.delim('data-raw/ph/evidence.txt', stringsAsFactors = F)
-save(ph_keys, file='data/ph_keys.Rdata')
+save(ph_keys, file='data/ph_keys.rdata')
 
 # Keys file (experimental design)
 ph_keys <- read.delim("data-raw/ph/keys.txt", stringsAsFactors = F)
-save(ph_evidence, file='data/ph_evidence.Rdata')
+save(ph_evidence, file='data/ph_evidence.rdata')
