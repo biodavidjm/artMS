24fc720bceaee766f8801474b0a6e677935f4b90
David Jimenez-Morales <biodavidjm@gmail.com>
2018-09-14 13:51:49 -0700

    Examples to evidenceToSaintExpressFormat
    #2

--- R/evidenceToSaintExpressFormat.R
+++ R/evidenceToSaintExpressFormat.R
@@ -2,23 +2,36 @@
 #' @title MaxQuant evidence file to SAINTexpress format
 #' 
 #' @description Converts the MaxQuant evidence file to the 3 required files 
-#' for SAINTexpress. One can choose to either use the `spectral counts`
-#' (use `spectral_count`) or the `intensities` (use `ms1`) for the analysis. 
-#' @param data_file Maxquant evidence file
-#' @param keys_file Keys file with a SAINT column specifying the test (`T`) and
-#' control (`C`) conditions
-#' @param ref_proteome_file Reference proteome file in fasta format
-#' @param quant_variable choose either `spectral_counts` (spectral counts) 
-#' or `ms1`
-#' @param output_file Output file name
+#' by SAINTexpress. One can choose to either use the `spectral counts`
+#' (use `msspc`) or the `intensities` (use `msint`) for the analysis. 
+#' @param input_file (char) Maxquant evidence file
+#' @param keys_file (char) Keys file with a SAINT column specifying 
+#' test (`T`) and control (`C`) conditions
+#' @param output_file (char) Output file name
+#' @param ref_proteome_file (char) Reference proteome path file name in 
+#' fasta format
+#' @param quant_variable (char) choose either 
+#' - `msspc` (spectral counts, default) or
+#' - `msint` (MS Intensity)
 #' @return The 3 required files by SAINTexpress
 #' @keywords SAINT, SAINTexpress, APMS
-#' artms_EvidenceToSaintExpressFormat()
+#' @examples \donttest{
+#' artms_evidenceToSaintExpressFormat(input_file = "a549-PB1-evidence.txt", 
+#'              keys_file = "a549-PB1-keys.txt", 
+#'              ref_proteome_file = "fluomics-uniprot-hsa_20170516.fasta", 
+#'              quant_variable = "msint", 
+#'              output_file = "a549-PB1-saintexpress.txt")
+#' }
 #' @export
-artms_evidenceToSaintExpressFormat <- function(data_file, keys_file, ref_proteome_file, quant_variable='spectral_count', output_file){
-  cat(">> CONVERTING TO SAINT FORMAT\n")
+artms_evidenceToSaintExpressFormat <- function(input_file, 
+                                               keys_file, 
+                                               ref_proteome_file, 
+                                               quant_variable='msspc', 
+                                               output_file){
   
-  data = fread(data_file, integer64 = 'double')
+  cat(">> CONVERTING TO SAINTexpress FORMAT\n")
+  
+  data = fread(input_file, integer64 = 'double')
   keys = fread(keys_file, integer64 = 'double')
   
   ## write baits in format
@@ -29,8 +42,8 @@ artms_evidenceToSaintExpressFormat <- function(data_file, keys_file, ref_proteom
   ## write interactions in format
   ## hIP101-10       PV_2C_co_uni    Q9NTG7  1
   
-  tryCatch(setnames(data, 'Raw file', 'RawFile'), error=function(e) cat('Raw.file not found\n'))
-  tryCatch(setnames(keys, 'Raw.file', 'RawFile'), error=function(e) cat('Raw.file not found\n'))
+  tryCatch(setnames(data, 'Raw file', 'RawFile'), error=function(e) cat("--- 'Raw file' not found\n"))
+  tryCatch(setnames(keys, 'Raw.file', 'RawFile'), error=function(e) cat("--- 'Raw.file' not found\n"))
   
   cat('>> VERIFYING DATA AND KEYS\n')
   if(any(!c('RawFile','IsotopeLabelType','Condition','BioReplicate','Run','SAINT') %in% colnames(keys))){
@@ -38,20 +51,20 @@ artms_evidenceToSaintExpressFormat <- function(data_file, keys_file, ref_proteom
   } 
   if(!'IsotopeLabelType' %in% colnames(data)) data[,IsotopeLabelType:='L']
   data <- artms_mergeMaxQDataWithKeys(data, keys, by = c('RawFile','IsotopeLabelType'))
-  data_f <- filterMaxqData(data)
-  data_f <- removeMaxQProteinGroups(data_f) ## do we want this or not?
+  data_f <- artms_filterMaxqData(data)
+  data_f <- .artms_removeMaxQProteinGroups(data_f) ## do we want this or not?
   
   cat(">> AGGREGATING ON", quant_variable,"VALUES...\n")
   ## aggregate over technical replicates if necessary
-  if(quant_variable == 'spectral_count'){
+  if(quant_variable == 'msspc'){
     setnames(data_f, 'MS/MS Count', 'spectral_counts')
     data_f_agg <- aggregate(spectral_counts ~ BioReplicate+Condition+Proteins+Sequence+Charge,data=data_f, FUN = max)
     data_f_agg <- aggregate(spectral_counts ~ BioReplicate+Condition+Proteins,data=data_f_agg, FUN = sum)
-  }else if(quant_variable=='ms1'){
+  }else if(quant_variable=='msint'){
     data_f_agg <- aggregate(Intensity ~ BioReplicate+Condition+Proteins+Sequence+Charge, data=data_f, FUN = max)
     data_f_agg <- aggregate(Intensity ~ BioReplicate+Condition+Proteins, data=data_f_agg,FUN = sum)
   }else{
-    stop("\nERROR!! Wrong value for variable to quantify. Please use 'spectral_count' or 'ms1'.")
+    stop("\nERROR!! Wrong value for variable to quantify. Please use 'msspc' or 'msint'")
   }
   
   ## IP name, bait name, prey name, and spectral counts or intensity values
@@ -83,7 +96,7 @@ artms_evidenceToSaintExpressFormat <- function(data_file, keys_file, ref_proteom
   missing_lengths = nrow(saint_preys[is.na(saint_preys$uniprot_id),])
   saint_preys[is.na(saint_preys$uniprot_id),]$uniprot_id=saint_preys[is.na(saint_preys$uniprot_id),]$uniprot_ac  
   if(missing_lengths>0){
-    cat(sprintf("\tWARNING!\tCOMPUTING %s MISSING LENGTHS WITH THE MEDIAN LENGTH FROM THE DATASET\n",missing_lengths))
+    cat(sprintf("--- WARNING! COMPUTING %s MISSING LENGTHS WITH THE MEDIAN LENGTH FROM THE DATASET\n",missing_lengths))
     saint_preys[is.na(saint_preys$lengths),]$lengths=median(saint_preys$lengths,na.rm = T)
   }
   
@@ -102,16 +115,4 @@ artms_evidenceToSaintExpressFormat <- function(data_file, keys_file, ref_proteom
   cat("--- ", gsub('.txt','-saint-interactions.txt', output_file, "\n"))
 }
 
-# ------------------------------------------------------------------------------
-#' @title Remove protein groups
-#' @description Remove the group of proteins ids separated by separated by `;`
-#' @param data Data.frame with a `Proteins` column.
-#' @return A data.frame with the protein groups removed
-#' @keywords maxquant, remove, proteingroups
-#' removeMaxQProteinGroups()
-#' @export
-removeMaxQProteinGroups <- function(data){
-  data_selected = data[grep(";",data$Proteins, invert=T),]
-  return(data_selected)
-}
 
