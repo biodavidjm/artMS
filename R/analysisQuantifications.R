18cc6e6d11eb5e00cf59afffad75d42e30540993
David JM <biodavidjm@gmail.com>
2019-02-18 11:05:06 -0800

    Updating annotations
    - New function to check whether a specie is supported
    - If specie is not supported, annotations are returned using uniprot ids
    - Enrichment analysis only for supported species
    - Fixed typos
    - Removed unnecessary messages
    
    #86 #87

--- R/analysisQuantifications.R
+++ R/analysisQuantifications.R
@@ -13,12 +13,20 @@
 #'
 #' @param log2fc_file (char) MSstats results file location
 #' @param modelqc_file (char) MSstats modelqc file location
-#' @param species (char) Main species. Currently supported: human, mouse
+#' @param species (char) Select one species. Species currently supported for
+#' a full analysis (including enrichment analysis):
+#' - HUMAN
+#' - MOUSE
+#' 
+#' To find out species supported only for annotation check 
+#' `?artmsIsSpeciesSupported()`
+#' 
 #' @param output_dir (char) Name for the folder to output the results from the 
 #' function. Default is current directory (recommended to provide a new folder
 #' name).
 #' @param enrich (logical) Performed enrichment analysis using GprofileR?
-#' `TRUE` (default) or `FALSE`
+#' Only available for species HUMAN and MOUSE. 
+#' `TRUE` (default if "human" or "mouse" are the species) or `FALSE`
 #' @param l2fc_thres (int) log2fc cutoff for enrichment analysis (default,
 #' `l2fc_thres = 1.5`)
 #' @param choosePvalue (char) specify whether `pvalue` or `adjpvalue` should 
@@ -149,10 +157,18 @@ artmsAnalysisQuantifications <- function(log2fc_file,
     stop("The < choosePvalue > argument is wrong. 
          The valid options are: <pvalue> or <adjpvalue> ")
   
+  if(!artmsIsSpeciesSupported(species)){
+    if(verbose) message("---(-) ", species, " is not supported. \n\tGene Symbol, Protein Name, and EntrezID won't be provided")
+  }
+  
+  # Because the species is known to be supported, let's keep going...
   species <- tolower(species)
-  if(!(species %in% c('human', 'mouse')))
-    stop("The < species > argument is wrong. 
-         The valid options are: <human> or <mouse> ")
+  if(!(species %in% c('human', 'mouse'))){
+    if(enrich){
+      message("-- Enrichment analysis turned off: only available for HUMAN and MOUSE")
+      enrich <- FALSE
+    }
+  }
   
   if (pathogen == "nopathogen") {
     if(verbose) message("--- No Pathogen extra in these samples (only Influenza) ")
@@ -192,6 +208,7 @@ artmsAnalysisQuantifications <- function(log2fc_file,
   # Removing the empty protein names
   if (any(dfmq$PROTEIN == "")) {
     dfmq <- dfmq[-which(dfmq$PROTEIN == ""), ]
+    if(verbose) message("--- Empty ID proteins removed from abundance data")
   }
   dfmq$PROTEIN <- gsub("(sp\\|)(.*)(\\|.*)", "\\2", dfmq$PROTEIN)
   dfmq$PROTEIN <- gsub("(.*)(\\|.*)", "\\1", dfmq$PROTEIN)
@@ -272,6 +289,7 @@ artmsAnalysisQuantifications <- function(log2fc_file,
     )
   if (any(dflog2fcraw$Protein == "")) {
     dflog2fcraw <- dflog2fcraw[-which(dflog2fcraw$Protein == ""), ]
+    if(verbose) message("--- Empty ID proteins removed from log2fc data")
   }
   dflog2fcraw$Protein <-
     gsub("(sp\\|)(.*)(\\|.*)", "\\2", dflog2fcraw$Protein)
@@ -671,7 +689,7 @@ artmsAnalysisQuantifications <- function(log2fc_file,
   OUTreprod[is.na(OUTreprod)] <- 0
   # Make a copy to use later
   bioReplicaInfo <- OUTreprod
-  # And geht the total number of biological replicates
+  # And get the total number of biological replicates
   OUTreprod$BiorepCount <- rowSums(OUTreprod[, 2:here])
   
   # Get whether a protein is found in all conditions
@@ -972,43 +990,46 @@ artmsAnalysisQuantifications <- function(log2fc_file,
   # ENRICHMENT OF MOST ABUNDANT PROTEINS (from IMPUTED LOG2FC values)
   # Let's select first significance based on pvalue, by using the iPvalue
   # we are already including the imputed pvalues...
+  
   l2fcol4enrichment <-
     data.table::dcast(data = imputedDF[which(imputedDF$iPvalue < 0.05), ], 
                       Protein ~ Label, value.var = 'iLog2FC')
   
-  if (dim(l2fcol4enrichment)[1] > 0) {
-    # Let's melt now for enrichment analysis
-    l2fcol4enrichment <-
-      reshape2::melt(
-        data = l2fcol4enrichment,
-        id.vars = c('Protein'),
-        variable.name = "Comparisons"
-      )
-    l2fcol4enrichment <-
-      l2fcol4enrichment[complete.cases(l2fcol4enrichment), ]
-    # Now get ready for annotation
-    if (grepl("ptm", isPtm)) {
-      names(l2fcol4enrichment)[grep('^Protein$', names(l2fcol4enrichment))] <-
-        'Uniprot_PTM'
-      # Take the Protein ID, but being very careful about the fluomics labeling
-      l2fcol4enrichment <- 
-        .artmsExtractUniprotId(x = l2fcol4enrichment, 
-                               uniprotPtmColumn = "Uniprot_PTM", 
-                               newColumnName = "Protein")
-      suppressMessages(
-        l2fcol4enrichment <-
-          artmsAnnotationUniprot(l2fcol4enrichment, 'Protein', species)
-      )
-    } else{
-      suppressMessages(
-        l2fcol4enrichment <-
-          artmsAnnotationUniprot(l2fcol4enrichment, 'Protein', species)
-      )
-    }
-  }
-  
   if (enrich == TRUE & dim(l2fcol4enrichment)[1] > 0) {
     if(verbose) message(">> ENRICHMENT ANALYSIS OF SELECTED CHANGES USING GPROFILER ")
+    
+
+    
+    if (dim(l2fcol4enrichment)[1] > 0) {
+      # Let's melt now for enrichment analysis
+      l2fcol4enrichment <-
+        reshape2::melt(
+          data = l2fcol4enrichment,
+          id.vars = c('Protein'),
+          variable.name = "Comparisons"
+        )
+      l2fcol4enrichment <-
+        l2fcol4enrichment[complete.cases(l2fcol4enrichment), ]
+      # Now get ready for annotation
+      if (grepl("ptm", isPtm)) {
+        names(l2fcol4enrichment)[grep('^Protein$', names(l2fcol4enrichment))] <-
+          'Uniprot_PTM'
+        # Take the Protein ID, but being very careful about the fluomics labeling
+        l2fcol4enrichment <- 
+          .artmsExtractUniprotId(x = l2fcol4enrichment, 
+                                 uniprotPtmColumn = "Uniprot_PTM", 
+                                 newColumnName = "Protein")
+        suppressMessages(
+          l2fcol4enrichment <-
+            artmsAnnotationUniprot(l2fcol4enrichment, 'Protein', species)
+        )
+      } else{
+        suppressMessages(
+          l2fcol4enrichment <-
+            artmsAnnotationUniprot(l2fcol4enrichment, 'Protein', species)
+        )
+      }
+    }
     
     if (grepl("ptm", isPtm)) {
       # l2fcol4enrichment <- 
@@ -1456,7 +1477,7 @@ artmsAnalysisQuantifications <- function(log2fc_file,
       )
   } else{
     stop(" WRONG isPTM SELECTED. 
-         OPTIONS AVAILABLE: global, ptmph, yesphsite ")
+         OPTIONS AVAILABLE: global, ptmph, ptmsites ")
   }
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -1679,71 +1700,76 @@ artmsAnalysisQuantifications <- function(log2fc_file,
                              annotation_legend_side = "right")
         garbage <- dev.off()
         
-        if(verbose) message("--- Enrichment analysis of the clusters ")
+        # Pre enrichment of clusters
         cl_number <- pam.res$clustering
         dfclusters <- as.data.frame(cl_number)
         dfclusters$ids <- row.names(dfclusters)
         dfclusters$Gene <- gsub("(.*)(_)(.*)", "\\1", dfclusters$ids)
         dfclusters$Protein <- gsub("(.*)(_)(.*)", "\\3", dfclusters$ids)
         
-        # Making sure we have unique genes in each comparison 
-        # (the PTM might bring redundancy)
-        pretmp <- dfclusters[c('Gene', 'cl_number')]
-        pretmp <- unique(pretmp)
-        
-        tmp <- split(pretmp$Gene, pretmp$cl_number, drop = TRUE)
-        
-        if (species == "human") {
-          enrichgenes <-
-            artmsEnrichProfiler(
-              tmp,
-              categorySource = c(
-                'GO:BP',
-                'GO:MF',
-                'GO:CC',
-                'KEGG',
-                'REAC',
-                'CORUM',
-                'HPA',
-                'OMIM'
-              ),
-              species = 'hsapiens',
-              listOfGenes,
-              verbose = verbose
-            ) # 'HP'
-        } else if (species == "mouse") {
-          enrichgenes <-
-            artmsEnrichProfiler(
-              tmp,
-              categorySource = c('GO:BP', 
-                                 'GO:MF', 
-                                 'GO:CC', 
-                                 'KEGG', 
-                                 'REAC', 
-                                 'CORUM'),
-              species = 'mmusculus',
-              listOfGenes,
-              verbose = verbose
-            )
-        } else{
-          stop(species, " is currently not supported in the enrichment")
+        if(enrich){
+          if(verbose) message("--- Enrichment analysis of the clusters ")
+          # Making sure we have unique genes in each comparison 
+          # (the PTM might bring redundancy)
+          pretmp <- dfclusters[c('Gene', 'cl_number')]
+          pretmp <- unique(pretmp)
+          
+          tmp <- split(pretmp$Gene, pretmp$cl_number, drop = TRUE)
+          
+          if (species == "human") {
+            enrichgenes <-
+              artmsEnrichProfiler(
+                tmp,
+                categorySource = c(
+                  'GO:BP',
+                  'GO:MF',
+                  'GO:CC',
+                  'KEGG',
+                  'REAC',
+                  'CORUM',
+                  'HPA',
+                  'OMIM'
+                ),
+                species = 'hsapiens',
+                listOfGenes,
+                verbose = verbose
+              ) # 'HP'
+          } else if (species == "mouse") {
+            enrichgenes <-
+              artmsEnrichProfiler(
+                tmp,
+                categorySource = c('GO:BP', 
+                                   'GO:MF', 
+                                   'GO:CC', 
+                                   'KEGG', 
+                                   'REAC', 
+                                   'CORUM'),
+                species = 'mmusculus',
+                listOfGenes,
+                verbose = verbose
+              )
+          } else{
+            stop(species, " is currently not supported in the enrichment")
+          }
+          
+          # Write the file
+          file_clusterheatenrich_l2fc <-
+            gsub(".txt",
+                 ".log2fc-clusterheatmap-enriched.txt",
+                 log2fc_file)
+          file_clusterheatenrich_l2fc <-
+            paste0(output_dir, "/", file_clusterheatenrich_l2fc)
+          write.table(
+            enrichgenes,
+            file_clusterheatenrich_l2fc,
+            col.names = TRUE,
+            row.names = FALSE,
+            sep = "\t",
+            quote = FALSE
+          )
         }
         
-        file_clusterheatenrich_l2fc <-
-          gsub(".txt",
-               ".log2fc-clusterheatmap-enriched.txt",
-               log2fc_file)
-        file_clusterheatenrich_l2fc <-
-          paste0(output_dir, "/", file_clusterheatenrich_l2fc)
-        write.table(
-          enrichgenes,
-          file_clusterheatenrich_l2fc,
-          col.names = TRUE,
-          row.names = FALSE,
-          sep = "\t",
-          quote = FALSE
-        )
-        
+        # Print out clusters
         file_clusterheatdata_l2fc <-
           gsub(".txt", ".log2fc-clusterheatmap.txt", log2fc_file)
         file_clusterheatdata_l2fc <-
@@ -1858,7 +1884,7 @@ artmsAnalysisQuantifications <- function(log2fc_file,
         "wide_iPvalue" = imputedDF_wide_pvalue
       )
     } else{
-      stop("UB is a not valid options")
+      stop(isPtm, " is not a valid option")
     }
   } else{
     stop("Contact developers")
@@ -1893,7 +1919,7 @@ artmsAnalysisQuantifications <- function(log2fc_file,
     if(verbose) message("- ENRICHMENT files should also be out ")
   }
   
-  if(verbose) message(" >> SUPER ANALYSIS IS DONE  ")
+  if(verbose) message(" >> SUPER ANALYSIS COMPLETED")
 }
 
 
--- R/annotations.R
+++ R/annotations.R
@@ -9,6 +9,7 @@
 #' @param columnid (char) The column with the uniprotkb ids
 #' @param species (char) The species name. Check `?artmsMapUniprot2Entrez`
 #' to find out more about supported species.
+#' @param verbose (logical) `TRUE` (default) shows function messages
 #' @return (data.frame) with two new columns: `Gene` and `Protein.name`
 #' @keywords annotation, uniprot
 #' @examples
@@ -19,7 +20,10 @@
 #'                                          columnid = 'Proteins',
 #'                                          species = 'human')
 #' @export
-artmsAnnotationUniprot <- function(x, columnid, species) {
+artmsAnnotationUniprot <- function(x, 
+                                   columnid, 
+                                   species,
+                                   verbose = TRUE) {
   
   if(any(missing(x) | 
          missing(columnid) |
@@ -32,38 +36,59 @@ artmsAnnotationUniprot <- function(x, columnid, species) {
   
   x <- .artms_checkIfFile(x)
   
-  theUniprots <- as.character(unique(x[[columnid]]))
+  # Deal with isoforms UNIPROT-1 etc
+  x$EntryRoot <- x[[columnid]]
   
-  preload <- artmsMapUniprot2Entrez(uniprotkb = theUniprots, 
-                                             species = species)
+  x$EntryRoot <- gsub("(\\w+)(-)(\\d+)", "\\1", x$EntryRoot)
   
-  dc_merged <-merge(x, 
-                    preload,
-                    by.x = columnid,
-                    by.y = "UNIPROT",
-                    all.x = TRUE)
+  theUniprots <- as.character(unique(x$EntryRoot))
   
-  # Move Gene name to the left:
-  gene_first <- preload[, c("SYMBOL", "UNIPROT", "GENENAME", "ENTREZID")]
-  dc_merged <- subset(dc_merged, select = -c(SYMBOL, GENENAME, ENTREZID))
-  send_back <-
-    merge(
-      gene_first,
-      dc_merged,
-      by.x = "UNIPROT",
-      by.y = columnid,
-      all.y = TRUE
-    )
-  names(send_back)[grep("^UNIPROT$", names(send_back))] <- "Protein"
-  names(send_back)[grep("^SYMBOL$", names(send_back))] <- "Gene"
-  names(send_back)[grep("^GENENAME$", names(send_back))] <- "ProteinName"
-  names(send_back)[grep("^ENTREZID$", names(send_back))] <- "EntrezID"
-  # Some uniprot entries might not have yet a gene name, 
-  # which will be an empty value. Replace with Entry name:
-  send_back$Gene[which(send_back$Gene == "")] <- NA
-  send_back$Gene[is.na(send_back$Gene)] <-
-    send_back$Protein[is.na(send_back$Gene)]
-  return(send_back)
+  if(isFALSE(artmsIsSpeciesSupported(species = species))){
+    if(verbose) message("---(-) Species ", species," not supported: no info about proteins will be provided")
+    keepSearchName <- paste0(columnid, "ori")
+    y <- artmsChangeColumnName(x, columnid, keepSearchName)
+    y <- artmsChangeColumnName(y, "EntryRoot", "Protein")
+    y$Gene <- y$Protein
+    y$ProteinName <- NA
+    y$EntrezID <- NA
+    send_back <- y[c("Protein", "Gene", "ProteinName", "EntrezID")]
+    y <- subset(y, select = -c(Gene, ProteinName, EntrezID))
+    send_back <- merge(send_back, y, by = "Protein")
+    return(send_back)
+  }else{
+    preload <- artmsMapUniprot2Entrez(uniprotkb = theUniprots, 
+                                      species = species)
+    
+    dc_merged <-merge(x, 
+                      preload,
+                      by.x = "EntryRoot",
+                      by.y = "UNIPROT",
+                      all.x = TRUE)
+    
+    # Move Gene name to the left:
+    gene_first <- preload[, c("SYMBOL", "UNIPROT", "GENENAME", "ENTREZID")]
+    dc_merged <- subset(dc_merged, select = -c(SYMBOL, GENENAME, ENTREZID))
+    send_back <-
+      merge(
+        gene_first,
+        dc_merged,
+        by.x = "UNIPROT",
+        by.y = "EntryRoot",
+        all.y = TRUE
+      )
+    keepSearchName <- paste0(columnid,"Ref")
+    send_back <- artmsChangeColumnName(send_back, columnid, keepSearchName)
+    names(send_back)[grep("^UNIPROT$", names(send_back))] <- "Protein"
+    names(send_back)[grep("^SYMBOL$", names(send_back))] <- "Gene"
+    names(send_back)[grep("^GENENAME$", names(send_back))] <- "ProteinName"
+    names(send_back)[grep("^ENTREZID$", names(send_back))] <- "EntrezID"
+    # Some uniprot entries might not have yet a gene name, 
+    # which will be an empty value. Replace with Entry name:
+    send_back$Gene[which(send_back$Gene == "")] <- NA
+    send_back$Gene[is.na(send_back$Gene)] <-
+      send_back$Protein[is.na(send_back$Gene)]
+    return(send_back)
+  }
 }
 
 # ------------------------------------------------------------------------------
@@ -72,36 +97,16 @@ artmsAnnotationUniprot <- function(x, columnid, species) {
 #' @description Map GENE SYMBOL, NAME, AND ENTREZID to a vector of Uniprot IDS
 #' @param uniprotkb (vector) Vector of UniprotKB IDs
 #' @param species (char) The species name. Species currently supported 
-#' as part of artMS:
-#' - HUMAN
-#' - MOUSE
-#' 
-#' And the following species can be used as well, but the user needs to 
-#' install the corresponding org.db package:
-#' - ANOPHELES (`install.packages(org.Ag.eg.db)`)
-#' - ARABIDOPSIS (`install.packages(org.At.tair.db)`)
-#' - BOVINE (`install.packages(org.Bt.eg.db)`)
-#' - WORM (`install.packages(org.Ce.eg.db)`)
-#' - CANINE (`install.packages(org.Cf.eg.db)`)
-#' - FLY (`install.packages(org.Dm.eg.db)`)
-#' - ZEBRAFISH (`install.packages(org.Dr.eg.db)`)
-#' - ECOLI_STRAIN_K12 (`install.packages(org.EcK12.eg.db)`)
-#' - ECOLI_STRAIN_SAKAI (`install.packages(org.EcSakai.eg.db)`)
-#' - CHICKEN (`install.packages(org.Gg.eg.db)`)
-#' - RHESUS (`install.packages(org.Mmu.eg.db)`)
-#' - MALARIA (`install.packages(org.Pf.plasmo.db)`)
-#' - CHIMP (`install.packages(org.Pt.eg.db)`)
-#' - RAT (`install.packages(org.Rn.eg.db)`)
-#' - YEAST (`install.packages(org.Sc.sgd.db)`)
-#' - PIG (`install.packages(org.Ss.eg.db)`)
-#' - XENOPUS (`install.packages(org.Xl.eg.db)`)
+#' as part of artMS: check `?artmsIsSpeciesSupported()` to find out the
+#' list of supported species`
 #' @return (data.frame) with ENTREZID and GENENAMES mapped on UniprotKB ids
 #' @keywords annotation, ids
 #' @examples
-#' # Load an example
+#' # Load an example with human proteins
 #' exampleID <- c("Q6P996", "B1N8M6")
 #' artmsMapUniprot2Entrez(uniprotkb = exampleID, 
 #'                        species = "HUMAN")
+#'                        
 #' @export
 artmsMapUniprot2Entrez <- function(uniprotkb, 
                                    species) {
@@ -113,24 +118,87 @@ artmsMapUniprot2Entrez <- function(uniprotkb,
   
   if(!is.vector(uniprotkb)) stop("Argument <uniprotkb> is not a vector")
   if(!is.character(species)) stop("Argument <species> is not a vector")
+
+  if(isFALSE(artmsIsSpeciesSupported(species = species))){
+    stop("Species ", species," not supported")
+  }else{
+    thePack <- artmsIsSpeciesSupported(species = species)
+  }
+
+  suppressMessages(
+    mappings <-
+      AnnotationDbi::select(
+        eval(as.symbol(thePack)),
+        uniprotkb,
+        c("UNIPROT", "SYMBOL",
+          "GENENAME", "ENTREZID"),
+        keytype = "UNIPROT"
+      )
+  )
+
+  mappings <- unique(mappings)
+  # It migth come with 1 uniprot to many gene names: 
+  # take the first one, which should be the main gene name
+  mappings <- mappings[!duplicated(mappings$UNIPROT),]
+  return(mappings)
+}
+
+
+# ------------------------------------------------------------------------------
+#' @title Check if a species is supported and available
+#'
+#' @description Given a species name, it checkes whether is supported, and 
+#' if supported, check whether the annotation package is installed.
+#' @param species (char) The species name. Species currently supported 
+#' as part of artMS:
+#' - HUMAN
+#' - MOUSE
+#' 
+#' And the following species can be used as well, but the user needs to 
+#' install the corresponding org.db package:
+#' - ANOPHELES (`install.packages(org.Ag.eg.db)`)
+#' - BOVINE (`install.packages(org.Bt.eg.db)`)
+#' - WORM (`install.packages(org.Ce.eg.db)`)
+#' - CANINE (`install.packages(org.Cf.eg.db)`)
+#' - FLY (`install.packages(org.Dm.eg.db)`)
+#' - ZEBRAFISH (`install.packages(org.Dr.eg.db)`)
+#' - CHICKEN (`install.packages(org.Gg.eg.db)`)
+#' - RHESUS (`install.packages(org.Mmu.eg.db)`)
+#' - CHIMP (`install.packages(org.Pt.eg.db)`)
+#' - RAT (`install.packages(org.Rn.eg.db)`)
+#' - YEAST (`install.packages(org.Sc.sgd.db)`)
+#' - PIG (`install.packages(org.Ss.eg.db)`)
+#' - XENOPUS (`install.packages(org.Xl.eg.db)`)
+#' @param verbose (logical) `TRUE` (default) shows function messages
+#' @return (string) Name of the package for the given species
+#' @keywords annotation, species
+#' @examples
+#' # Should return TRUE
+#' artmsIsSpeciesSupported(species = "HUMAN")
+#' artmsIsSpeciesSupported(species = "CHIMP")
+#' @export
+artmsIsSpeciesSupported <- function(species, 
+                                    verbose = TRUE) {
+  
+  if(any(missing(species)))
+    stop("Missed (one or many) required argument(s)
+         Please, check the help of this function to find out more")
+  
+  if(!is.character(species)) stop("Argument <species> is not a vector")
   
   species <- toupper(species)
   
   LongName <- c(
     "ANOPHELES", 
-    "ARABIDOPSIS",
     "BOVINE",
     "WORM",
     "CANINE",
     "FLY",
     "ZEBRAFISH",
-    "ECOLI_STRAIN_K12",
-    "ECOLI_STRAIN_SAKAI",
     "CHICKEN",
     "HUMAN",
     "MOUSE",
     "RHESUS",
-    "MALARIA",
     "CHIMP",
     "RAT",
     "YEAST",
@@ -138,19 +206,15 @@ artmsMapUniprot2Entrez <- function(uniprotkb,
     "XENOPUS")
   PackageName <- c(
     "org.Ag.eg.db", 
-    "org.At.tair.db",
     "org.Bt.eg.db",
     "org.Ce.eg.db",
     "org.Cf.eg.db",
     "org.Dm.eg.db",
     "org.Dr.eg.db",
-    "org.EcK12.eg.db",
-    "org.EcSakai.eg.db",
     "org.Gg.eg.db",
     "org.Hs.eg.db",
     "org.Mm.eg.db",
     "org.Mmu.eg.db",
-    "org.Pf.plasmo.db",
     "org.Pt.eg.db",
     "org.Rn.eg.db",
     "org.Sc.sgd.db",
@@ -164,29 +228,18 @@ artmsMapUniprot2Entrez <- function(uniprotkb,
   if(species %in% OrgDB$LongName){
     thePack <- OrgDB$PackageName[which(OrgDB$LongName == species)]
     
-    if( !(thePack %in% rownames(installed.packages())) )
-      stop("---(-) The package <",thePack,"> is not installed in your system.
-           Just run: install.packages('",thePack,"') and try again")
-    
-    suppressMessages(
-      mappings <-
-        AnnotationDbi::select(
-          eval(as.symbol(thePack)),
-          uniprotkb,
-          c("UNIPROT", "SYMBOL",
-            "GENENAME", "ENTREZID"),
-          keytype = "UNIPROT"
-        )
-    )
+    if( !(thePack %in% rownames(installed.packages())) ){
+      if(verbose) message("---(-) The package <",thePack,"> is not installed in your system.
+           Just run: install.packages('", thePack,"') and try again")
+      return(FALSE)
+    } else{
+      return(thePack)
+    }
   }else{
-    stop("Specie ", species, " not supported. 
-         Please, check help to find out more about supported species")
+    return(FALSE)
   }
-  
-  mappings <- unique(mappings)
-  # It migth come with 1 uniprot to many gene names: 
-  # take the first one, which should be the main gene name
-  mappings <- mappings[!duplicated(mappings$UNIPROT),]
-  return(mappings)
 }
+
+
+
 
