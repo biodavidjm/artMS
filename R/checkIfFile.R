0d786868f8b18dfe966c6dc16f7e115fbf2d7f81
Ben Polacco <bpolacco@gmail.com>
2020-04-09 12:02:49 -0700

    fix dont_check_names default and docstring; add a dropped x <-

--- R/checkIfFile.R
+++ R/checkIfFile.R
@@ -4,15 +4,15 @@
 #  either a path to a data file or a data object in data.frame or data.table
 #  form.
 # @param input_file (object or data.frame) The filepath/object to be checked.
-# @param dont_check_names if TRUE (default) names are not checked, otherwise
-# names will be checked
+# @param dont_check_names if TRUE names are not checked, otherwise
+# names will be checked (default)
 # @param is.evidence (logical) Whether or not the file to be read in is an
 # evidence file. This will assign proper classes to the evidence file
 # when being read in.
 # @return An R data object
 # @keywords internal, file, evidence, input
 .artms_checkIfFile <- function(input_file, 
-                               dont_check_names = TRUE,
+                               dont_check_names = FALSE,
                                is.evidence = FALSE) {
   # check if already a data.frame or data.table
   if (is.data.table(input_file)) {
@@ -24,9 +24,9 @@
       if(!file.exists(input_file)){
         stop("The file ", input_file, " does not exist! ")
       }else{
-        read.delim(input_file,
-                   stringsAsFactors = FALSE,
-                   check.names = !dont_check_names)
+        x <- read.delim(input_file,
+                        stringsAsFactors = FALSE,
+                        check.names = !dont_check_names)
       }
     }else{
       stop("The input object is not valid")
